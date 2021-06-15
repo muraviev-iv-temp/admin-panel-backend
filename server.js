76d71d46efc66e4303fcd28120fb42aede21ec67
muraviev_iv <ivlmuravyev@sberbank.ru>
2021-06-15 12:46:47 +0300

    добавлено удаление

--- server.js
+++ server.js
@@ -26,18 +26,42 @@ app.get('/', (req, res) => {
     res.send(JSON.stringify(data))
 })
 
+const removeIds = (source, ids) => {
+  return source.filter(element => !ids.find(id => id == element.id))
+}
+
 app.post('/', bodyParser.json(), (req, res) => {
   const { body } = req;
     if(body.do) {
       body.do.forEach(element => {
+        data[element.name] && element.delete && (
+          data[element.name] = removeIds(data[element.name], element.delete)
+        )
         console.log(element)
       });
     }
     const response = {};
     if(body.get) {
       body.get.forEach(query => {
-        console.log('data', query,data[query])
-        response[query] = data[query]
+        switch (typeof query) {
+          case 'string':
+            response[query] = data[query];
+            break;
+          case 'object':
+            if (query.name === 'orders') {
+              const scope = data[query.name];
+              const targetPagesCount = scope.length > 0 && query.pageNum 
+                && Math.ceil(scope.length / query.pageCapacity) 
+              response[query.name] = {
+                orders: scope.slice(start = (query.pageNum - 1) * query.pageCapacity, start + query.pageCapacity),
+                pageNum: query.pageNum,
+                pagesCount: targetPagesCount > 0 && targetPagesCount
+              }
+            }
+            break;
+          default: 
+            throw new Exception('unexpected query type')
+        }
       })
     }
     res.send(JSON.stringify(response))
